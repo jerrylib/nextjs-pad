f2a02b1a9e348e7642b2ddf4a2b739032393cc00
李彬 <180505@nd.com>
2022-05-30 18:30:08 +0800

    chore: config update

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 
-const { PHASE_PRODUCTION_BUILD } = require('next/constants')
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 module.exports = phase => {
-  if (phase === PHASE_PRODUCTION_BUILD)
+  if (phase === PHASE_DEVELOPMENT_SERVER)
     return {
+      env: {
+        env_index: 'local',
+        MONGO_DB_URL: ''
+      }
+    }
+  return {
+    env: {
       env_index: 'pro',
       MONGO_DB_URL: ''
     }
-  return {
-    env_index: 'local',
-    MONGO_DB_URL: ''
   }
 }
