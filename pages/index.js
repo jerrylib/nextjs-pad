93c54e7e98a00470ee2b32854105b4ecab4556db
韩旭 <js_bin@163.com>
2022-07-12 15:58:01 +0800

    refactor : code update

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from "react";
 
 // === Utils === //
-import { useRequest } from '@umijs/hooks'
-import map from 'lodash/map'
+import { useRequest } from "@umijs/hooks";
+import map from "lodash/map";
 
 // === Styles === //
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
-export default function Home () {
-  const resp = useRequest(() => fetch('/api/events').then(rs => rs.json()))
-  const { data, refresh } = resp
+export default function Home() {
+  const resp = useRequest(() => fetch("/api/events").then((rs) => rs.json()));
+  const { data, refresh } = resp;
   return (
     <div className={styles.container}>
-      {map(data, i => (
+      {map(data, (i) => (
         <p key={i._id}>
           {i._id},{i.emails}
         </p>
       ))}
       <button
-        onClick={() => fetch('/api/events', { method: 'POST' }).then(refresh)}
+        onClick={() => fetch("/api/events", { method: "POST" }).then(refresh)}
       >
-        add
+        add11
       </button>
     </div>
-  )
+  );
 }
