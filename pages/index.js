410d5838ed02d6c48cd8a3f9a4bb6599073dad80
jiangcx <786255@nd.com>
2022-07-15 19:18:12 +0800

    feat: import web3

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,12 @@ import map from 'lodash/map'
 
 // === Styles === //
 import styles from '../styles/Home.module.css'
+import Web3 from 'web3'
+import { VAULT_ABI } from '../abis'
+
+const web3 = new Web3("https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161")
+const vaultContract = new web3.eth.Contract(VAULT_ABI, '0xd5C7A01E49ab534e31ABcf63bA5a394fF1E5EfAC')
+vaultContract.methods.getStrategies().call().then(console.log)
 
 export default function Home () {
   const resp = useRequest(() => fetch('/api/events').then(rs => rs.json()))
