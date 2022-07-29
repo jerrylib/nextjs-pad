ef2e11b57366c7eb0bdf76fe56bf5e9891c4a39d
韩旭 <js_bin@163.com>
2022-07-29 14:40:22 +0800

    feature: add receipt decoder

---
+++ utils/decode-util.js
@@ -0,0 +1,25 @@
+// === Utils === //
+import abiDecoder from "abi-decoder";
+
+// === Constants === //
+import {
+  IERC20_ABI,
+  ONINCH_ABI,
+  STRATEGY_ABI,
+  VAULT_ABI,
+  UNKNOW_ABI,
+} from "./../abis/index";
+
+abiDecoder.addABI(VAULT_ABI);
+abiDecoder.addABI(IERC20_ABI);
+abiDecoder.addABI(STRATEGY_ABI);
+abiDecoder.addABI(ONINCH_ABI);
+abiDecoder.addABI(UNKNOW_ABI);
+
+export const decodeLogs = (receipt = { logs: [] }) => {
+  return abiDecoder.decodeLogs(receipt.logs);
+};
+
+export const decodeMethod = (data) => {
+  return abiDecoder.decodeMethod(data);
+};
