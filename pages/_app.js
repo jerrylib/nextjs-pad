b12ebde7fb88e86529227abac8a7f97b82b604d3
韩旭gogogo <js_bin@163.com>
2022-09-21 17:26:05 +0800

    feature: add echarts

--- pages/_app.js
+++ pages/_app.js
@@ -5,34 +5,39 @@ import { Row, Col, Segmented } from 'antd'
 import { ReducerContextProvider } from '../reducer/web3Reducer'
 import { BarsOutlined, AppstoreOutlined } from '@ant-design/icons'
 
+// === Utils === //
+import get from 'lodash/get'
+
 import 'antd/dist/antd.css'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
+  const isFull = get(router, 'query.full', false)
   return (
     <ReducerContextProvider>
       <Head>
         <title>Bank Of Chain Tools</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/echarts/2.2.7/echarts-all.js"></script>
       </Head>
-      <Row gutter={[12, 12]} style={{ padding: '24px 0 0 24px' }}>
-        <Col span={24}>
-          <Segmented
-            value={router.route}
-            options={[
-              {
-                value: '/',
-                icon: <BarsOutlined onClick={() => router.push('/')} />
-              },
-              {
-                value: '/scan',
-                icon: <AppstoreOutlined onClick={() => router.push('/scan')} />
-              }
-            ]}
-          />
-        </Col>
-      </Row>
+      {!isFull && (
+        <Row gutter={[12, 12]} style={{ padding: '24px 0 0 24px' }}>
+          <Col span={24}>
+            <Segmented
+              value={router.route}
+              options={[
+                {
+                  value: '/',
+                  icon: <BarsOutlined onClick={() => router.push('/')} />
+                },
+                {
+                  value: '/scan',
+                  icon: <AppstoreOutlined onClick={() => router.push('/scan')} />
+                }
+              ]}
+            />
+          </Col>
+        </Row>
+      )}
       <Component {...pageProps} />
     </ReducerContextProvider>
   )
