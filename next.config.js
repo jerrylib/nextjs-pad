0518c503531de4d4ab6425d0b7a7134fab574c2d
韩旭gogogo <js_bin@163.com>
2023-03-17 16:18:31 +0800

    chore: tailwindcss add

--- next.config.js
+++ next.config.js
@@ -1,30 +1,17 @@
-const UnoCSS = require('@unocss/webpack').default
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: config => {
-    config.cache = false
-    config.plugins.push(UnoCSS())
-    return config
+module.exports = phase => {
+  if (phase === PHASE_DEVELOPMENT_SERVER)
+    return {
+      env: {
+        env_index: 'local',
+        MONGO_DB_URL: 'mongodb+srv://boccc:hPqOfVfct6yMnYx8@cluster0.clfqb.mongodb.net/local-db?retryWrites=true&w=majority'
+      }
+    }
+  return {
+    env: {
+      env_index: 'pro',
+      MONGO_DB_URL: 'mongodb+srv://boccc:hPqOfVfct6yMnYx8@cluster0.clfqb.mongodb.net/pro-db?retryWrites=true&w=majority'
+    }
   }
 }
-
-module.exports = nextConfig
-
-// TODO: legacy config for mogodb
-// module.exports = phase => {
-//   if (phase === PHASE_DEVELOPMENT_SERVER)
-//     return {
-//       env: {
-//         env_index: 'local',
-//         MONGO_DB_URL: 'mongodb+srv://boccc:hPqOfVfct6yMnYx8@cluster0.clfqb.mongodb.net/local-db?retryWrites=true&w=majority'
-//       }
-//     }
-//   return {
-//     env: {
-//       env_index: 'pro',
-//       MONGO_DB_URL: 'mongodb+srv://boccc:hPqOfVfct6yMnYx8@cluster0.clfqb.mongodb.net/pro-db?retryWrites=true&w=majority'
-//     }
-//   }
-// }
