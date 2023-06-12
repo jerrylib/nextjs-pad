2d1353692b6b68bb12566710144937fc90391d80
韩旭gogogo <js_bin@163.com>
2023-06-12 18:21:44 +0800

    refactor: code update

--- src/pages/ethers/index.jsx
+++ src/pages/ethers/index.jsx
@@ -8,28 +8,28 @@ import Demo2 from './components/Demo2'
 // === Utils === //
 import { map } from 'lodash'
 
-const demoArray = [<Demo1 />, <Demo2 />]
-
 let url = 'https://remix.ethereum.org/#lang=en&optimize=false&runs=200&evmVersion=null&version=soljson-v0.8.18+commit.87f61d96.js'
 url = 'https://replit.com?lite=true'
 url = 'https://replit.com/@jerrylib/goodboy?embed=true&edit=true'
 
+const demoArray = [
+  <Row>
+    <Col span={12} className="p-4">
+      章节目录
+      <li>章节目录</li>
+      <li>章节目录</li>
+      <li>章节目录</li>
+    </Col>
+    <Col span={12}>
+      <iframe className="w-full h-64" src={url} frameBorder="0"></iframe>
+    </Col>
+  </Row>,
+  <Demo1 />
+]
+
 const Ethers = () => {
   return (
     <Row className="w-full p-4">
-      <Col span={24} className="border border-solid border-red-400">
-        <Row>
-          <Col span={12} className="p-4">
-            章节目录
-            <li>章节目录</li>
-            <li>章节目录</li>
-            <li>章节目录</li>
-          </Col>
-          <Col span={12}>
-            <iframe className="w-full h-64" src={url} frameBorder="0"></iframe>
-          </Col>
-        </Row>
-      </Col>
       {map(demoArray, (item, index) => {
         return (
           <Col key={index} span={24} className="border border-solid border-red-400 mt-1">
